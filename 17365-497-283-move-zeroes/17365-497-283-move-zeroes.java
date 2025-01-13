2032098710120a73bcc04a320bfb9475077a8345
Sarveshas <138692847+sarveshshirulkar@users.noreply.github.com>
2025-01-13 23:01:35 +0530

    Time: 2 ms, Memory: 46 MB - LeetHub

---
+++ 17365-497-283-move-zeroes/17365-497-283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int snowBallSize = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0){
+                snowBallSize++;
+            }else if(snowBallSize > 0){
+                nums[i-snowBallSize] = nums[i];
+                nums[i] = 0;
+            }
+        }
+        return;
+    }
+}
