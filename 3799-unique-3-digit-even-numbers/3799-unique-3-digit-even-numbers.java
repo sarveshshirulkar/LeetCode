9fe2caee86596b11d84a12d7052cf239a6381331
Sarveshas <138692847+sarveshshirulkar@users.noreply.github.com>
2025-05-12 12:40:23 +0530

    Time: 10 ms (13.75%), Space: 43.2 MB (93.17%) - LeetHub

---
+++ 3799-unique-3-digit-even-numbers/3799-unique-3-digit-even-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        List<Integer> list = new ArrayList<>();
+        int[] freq = new int[10];
+        for(int d : digits) freq[d]++;
+
+        for(int i=100; i<999; i += 2){
+            int a = i / 100;  // zeros place
+            int b = (i / 10) % 10; //ones
+            int c = i % 10; // tens
+
+            freq[a]--; freq[b]--; freq[c]--;
+            if(freq[a] >= 0 && freq[b] >= 0 && freq[c] >= 0) list.add(i); 
+            freq[a]++; freq[b]++; freq[c]++;
+
+        }
+        return list.size();
+    }
+}
