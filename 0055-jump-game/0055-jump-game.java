781a1311c25404397e5be3beebb8b99141d4884b
Sarveshas <138692847+sarveshshirulkar@users.noreply.github.com>
2025-07-15 20:11:36 +0530

    Time: 1 ms (99.82%), Space: 45.5 MB (84.71%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int target = n-1;
+
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]+i >= target){
+                target = i;
+            }
+        }
+        return target == 0;
+    }
+}
